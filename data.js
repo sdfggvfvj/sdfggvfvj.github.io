c50266b7aabe9c632486c9bda07d9988b787bd33
test-git <imnotgayyyyy@gmail.com>
2020-02-16 03:29:59 +0100

    -

---
+++ data.js
@@ -0,0 +1,24 @@
+var data = {
+  "works":[
+
+      {
+        "image": "thefusion-2.jpeg",
+        "titles": "www.youtube.com/watch?v=egN_TEU72Ok&t=43s",
+        "description": "a video about dreams and memories",
+        "maker": "name"
+      },
+      {
+        "image": "lily.png",
+        "titles": "www.youtube.com/watch?v=1wFbWfACKco",
+        "description": "a fictional documentary about a photographer whose name is Lily.",
+        "maker": "name"
+      },
+
+      {
+        "image": "artificial.png",
+        "titles": "artificial landscape",
+        "description": "creating a way of generating pictures",
+        "maker": "name"
+      }
+    ]
+}
