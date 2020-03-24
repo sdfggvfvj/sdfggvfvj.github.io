fc80bc75a35171414a7d2373ffbb15fc1b77409d
test-git <imnotgayyyyy@gmail.com>
2020-03-24 19:21:05 +0100

    -

--- data.js
+++ data.js
@@ -4,7 +4,6 @@ var data = {
         "image": "thefusion-2.jpeg",
         "titles": "the fusion",
         "description": "a video about dreams and memories",
-        "maker": "name",
         "url": "https://www.youtube.com/watch?v=egN_TEU72Ok&t=43s"
       },
 
@@ -12,7 +11,6 @@ var data = {
         "image": "lily.png",
         "titles": "Lily's Photographs",
         "description": "a fictional documentary about a photographer whose name is Lily.",
-        "maker": "name",
         "url": "https://www.youtube.com/watch?v=1wFbWfACKco"
       },
 
@@ -20,7 +18,6 @@ var data = {
         "image": "glittering.jpg",
         "titles": "artificial 'Luxury'",
         "description": "an image making collaboration with Sangah Shin, Presented at Seoul Art Book Fair, 2019",
-        "maker": "name",
         "url": ''
       },
 
@@ -28,7 +25,6 @@ var data = {
         "image": "qiaohetang-1.jpg",
         "titles": "TaoBaoTu made for QiaoHeTang",
         "description": "I made two images for Qiaochu Guo's Project - QiaoHeTang, presented at Temporary Art Center Eindhoven, 2019",
-        "maker": "name",
         "url": 'details/taobaotu_for_qiaohetang.html'
       },
 
@@ -36,15 +32,20 @@ var data = {
         "image": "artificial.jpg",
         "titles": "artificial landscape",
         "description": "creating a way of generating pictures",
-        "maker": "name",
         "url": 'details/artificial_landscapes.html'
+      },
+
+      {
+        "image": "pictypes02.jpg",
+        "titles": "pictype",
+        "description": "a typeface made of daily images",
+        "url": "https://drive.google.com/file/d/1MpDXtHwaTOAUOOap-cMPpB-f6EQucpfT/view?usp=sharing"
       },
 
       {
         "image": "human.jpg",
         "titles": "who is she?",
         "description": "idk",
-        "maker": "name",
         "url": ''
       },
 
@@ -53,7 +54,6 @@ var data = {
             "image": "keying-1.png",
             "titles": "keying-practice",
             "description": "a practice about mixing videos",
-            "maker": "name",
             "url": "https://www.youtube.com/watch?v=os1Wx-qOOvw"
           }
 
