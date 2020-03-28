a9c1cb708615cbbe57d6bd34c53cd00dd64fe2c6
test-git <imnotgayyyyy@gmail.com>
2020-03-28 20:27:17 +0100

    -

--- test/data.js
+++ test/data.js
@@ -1,17 +1,18 @@
 var data = {
   "works":[
+
+    {
+
+      "v_titles": "cheawon shin",
+      "v_description": "Cheawon Shin and Sumin Choi made this work and all right reserved to them. / By using our magic power, we want to give a fascinating world to people.",
+      "v_src":"https://www.youtube.com/embed/3CEeWTfgNSw"
+    },
+
     {
         "v_titles": "xiaoyuan gao",
         "v_description": "a video about dreams and memories",
         "v_src":"https://www.youtube.com/embed/1wFbWfACKco",
    },
-
-          {
-
-            "v_titles": "cheawon shin",
-            "v_description": "Cheawon Shin and Sumin Choi made this work and all right reserved to them. / By using our magic power, we want to give a fascinating world to people.",
-            "v_src":"https://www.youtube.com/embed/3CEeWTfgNSw"
-          },
 
           {
 
@@ -27,6 +28,14 @@ var data = {
             "v_titles": "lu lin",
             "v_description": "The project “me, object” is an interactive game which consists of the processes of exploring personality and identity. With this game I want to trigger the participants to experience a journey of self-discovery. In the game, each person will individually question their openness, sense of beauty, joy, emotion, value for fashion, trust and examine their own ego.",
             "v_src":"https://www.youtube.com/embed/TnL3EUm3fDw"
+          },
+
+
+          {
+
+            "v_titles": "kuan-ting chen",
+            "v_description": "Puntuations as soil, and let the alphabets grow above. Punctuations as soil, and let the alphabets grow above.",
+            "v_src":"websites/kc/sr_twigs.html"
           }
 
 
@@ -37,9 +46,9 @@ var data = {
 
 var picsdata = {
   "photos":[
-    {   "p_images": "1.jpg",
-        "p_descriptions": "test pic",
-        "p_titles": "hah",
+    {   "p_images": "human.jpg",
+        "p_descriptions": "who is she?",
+        "p_titles": "xiaoyuan gao",
         "p_src":"https://www.youtube.com/embed/1wFbWfACKco"
 
       }
