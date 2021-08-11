f9f472ac4843d6eb2a63c927842695a77340628c
test-git <imnotgayyyyy@gmail.com>
2021-08-11 17:06:18 +0200

    -

---
+++ replace/findAndReplaceDOMText-master/replace.js
@@ -0,0 +1,23 @@
+findAndReplaceDOMText(document.body, {
+  find: 'o',
+  wrap: 'span',
+  wrapClass:'yes',
+});
+
+findAndReplaceDOMText(document.body, {
+  find: 'r',
+  wrap: 'span',
+  wrapClass:'letter-r',
+});
+
+findAndReplaceDOMText(document.body, {
+  find: 'p',
+  wrap: 'span',
+  wrapClass:'letter-p',
+});
+
+findAndReplaceDOMText(document.body, {
+  find: 's',
+  wrap: 'span',
+  wrapClass:'letter-s',
+});
