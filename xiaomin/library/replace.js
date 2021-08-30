73071427eda923f953a122f736c2826f6c5b5f91
test-git <imnotgayyyyy@gmail.com>
2021-08-30 15:10:24 +0200

    -

--- xiaomin/library/replace.js
+++ xiaomin/library/replace.js
@@ -3,3 +3,20 @@ findAndReplaceDOMText(document.body, {
   wrap: 'em',
   wrapClass:'letter-m',
 });
+
+findAndReplaceDOMText(document.body, {
+  find: 'o',
+  wrap: 'em',
+  wrapClass:'letter-o',
+});
+
+findAndReplaceDOMText(document.body, {
+  find: 's',
+  wrap: 'em',
+  wrapClass:'letter-s',
+});
+findAndReplaceDOMText(document.body, {
+  find: 'd',
+  wrap: 'em',
+  wrapClass:'letter-d',
+});
