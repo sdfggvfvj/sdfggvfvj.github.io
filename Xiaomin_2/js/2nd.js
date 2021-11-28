af43d9d2c2521e0e4ba1136239e5e2f85c7eaf7b
test-git <imnotgayyyyy@gmail.com>
2021-11-28 19:48:54 +0100

    -

--- Xiaomin_2/js/2nd.js
+++ Xiaomin_2/js/2nd.js
@@ -1,17 +1,11 @@
-window.onload=function(){
-    var mytext = document.getElementsByClassName("mytext");
-
-    for(var i=0; i<mytext.length; i++){
-        mytext[i].addEventListener("click",function(){
-
-//                   check the src on console
-            console.log(this.src);
-//                   add one
-          document.getElementById("test2").innerHTML='<img src="'+ this.src +'">';
-//                   add a lot
-/*                        var img = document.createElement('img');
-            img.src = this.src;
-            document.getElementById('test').appendChild(img);*/
-        })
-    }
+function showText() {
+  var x = document.getElementById("mytext");
+      y = document.getElementById("test");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "flex";
+  }
 }
