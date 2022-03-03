93463f30a2e0fecb89d901325e821bfc8e2b3c91
test-git <imnotgayyyyy@gmail.com>
2022-03-03 20:59:27 +0100

    Delete slider.js

--- typefaces/slider.js
+++
@@ -1,6 +0,0 @@
-function changeSize() {
-  let size = document.getElementById('rangeSize').value;
-  console.log(size);
-  let txtsize = size + 'pt';
-  document.getElementById('txt').style.fontSize = txtsize;
-}
